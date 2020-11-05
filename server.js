27116bb90a024bdcd9b3d5098e9781ca77fbdb04
Udaya N <udayaprakash2899@gmail.com>
2020-11-05 22:28:24 +0530

    added cors support

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var ghost = require("ghost");
 var express = require("express");
 var urlService = require("./node_modules/ghost/core/frontend/services/url");
+var cors = require("cors");
 var parentApp = express();
 
 // Run a single Ghost process
 ghost()
-  .then(function(ghostServer) {
+  .then(function (ghostServer) {
     // for making subdir work
+    parentApp.use(cors());
     parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
     ghostServer.start(parentApp);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
   });
